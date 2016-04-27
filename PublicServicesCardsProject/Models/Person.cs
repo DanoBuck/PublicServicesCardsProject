f903c6c05c763583d6be521d00cd6fa90dcefd0a
DanoBuck <X00109141@gmail.com>
2016-04-27 20:58:13 +0100

    Models Added Which Needs Further Attributes in the Future - Implemented with Inheritence

---
+++ PublicServicesCardsProject/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicServicesCardsProject.Models
+{
+    class Person
+    {
+        public virtual int PersonId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual string EmailAddress { get; set; }
+    }
+}
---
+++ PublicServicesCardsProject/Models/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicServicesCardsProject.Models
+{
+    class Staff : Person
+    {
+        public virtual double Salary { get; set; }
+        public virtual int DeskNumber { get; set; }
+    }
+}
