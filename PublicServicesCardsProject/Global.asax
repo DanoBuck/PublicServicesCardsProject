279c8913c8324cbe03c3dbe230401f999048f50e
DanoBuck <X00109141@gmail.com>
2016-04-27 19:46:34 +0100

    First Commit. Creation of Initial Project

---
+++ PublicServicesCardsProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PublicServicesCardsProject.MvcApplication" Language="C#" %>
---
+++ PublicServicesCardsProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PublicServicesCardsProject.Startup))]
+namespace PublicServicesCardsProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
