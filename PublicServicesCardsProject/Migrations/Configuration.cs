20bab4df69f0874e289f859719dd925311b1d485
DanoBuck <X00109141@gmail.com>
2016-04-30 12:39:14 +0100

    1 Seed Data Inserted to Configuration File

--- PublicServicesCardsProject/Migrations/Configuration.cs
+++ PublicServicesCardsProject/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace PublicServicesCardsProject.Migrations
 {
+    using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -26,6 +28,47 @@ namespace PublicServicesCardsProject.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var Staff = new List<Staff>()
+            {
+               new Staff
+               {
+                   FirstName = "Daniel",
+                   LastName = "Buckley",
+                   DateOfBirth = new DateTime(1995,1,9),
+                   EmailAddress = "DaniielBuckleyTy3@gmail.com",
+                   PPSN = "1234567Q",
+                   Salary = 45000,
+                   DeskNumber = 1,
+                   Building = new Building
+                   {
+                       BuildingId = 1
+                       // Staff Needs To Be Populated
+                   },
+                   Appointments = new List<Appointment>
+                   {
+                       new Appointment
+                       {
+                           AppointmentId = 1,
+                           DateOfAppointment = DateTime.Today.Date,
+                           TimeOfAppointment = DateTime.Now.AddHours(2),
+                           Customer = new Customer
+                           {
+                               FirstName = "Adam",
+                               LastName = "Buckley",
+                               DateOfBirth = new DateTime(1996,12,2),
+                               EmailAddress = "Adam@gmail.com",
+                               PPSN = "1234567S",
+                               CivilStatus = "Single"
+                           },
+                           Building = new Building
+                           {
+                               BuildingId = 1
+                               // Staff Needs To Be Populated
+                           }
+                       }
+                   }
+               }
+            };
         }
     }
 }
