5cb0832183f38b6cbe0e7d4f42f3518b244b0d5f
DanoBuck <X00109141@gmail.com>
2016-04-30 19:04:15 +0100

    Seed Method Updated to be Cleaner for Test Data

--- PublicServicesCardsProject/Migrations/Configuration.cs
+++ PublicServicesCardsProject/Migrations/Configuration.cs
@@ -12,7 +12,8 @@ namespace PublicServicesCardsProject.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(PSCContext context)
@@ -33,6 +34,7 @@ namespace PublicServicesCardsProject.Migrations
             {
                new Staff
                {
+                   PersonId = 1,
                    FirstName = "Daniel",
                    LastName = "Buckley",
                    DateOfBirth = new DateTime(1995,1,9),
@@ -40,33 +42,83 @@ namespace PublicServicesCardsProject.Migrations
                    PPSN = "1234567Q",
                    Salary = 45000,
                    DeskNumber = 1,
-                   Building = new Building
-                   {
-                       BuildingId = 1
-                       // Staff Needs To Be Populated
-                   },
-                   Appointments = new List<Appointment>
-                   {
-                       new Appointment
-                       {
-                           AppointmentId = 1,
-                           DateOfAppointment = DateTime.Today.Date,
-                           TimeOfAppointment = DateTime.Now.AddHours(2),
-                           Customer = new Customer
-                           {
-                               FirstName = "Adam",
-                               LastName = "Buckley",
-                               DateOfBirth = new DateTime(1996,12,2),
-                               EmailAddress = "Adam@gmail.com",
-                               PPSN = "1234567S",
-                               CivilStatus = "Single"
-                           }
-                       }
-                   }
+               },
+               new Staff
+               {
+                   PersonId = 2,
+                   FirstName = "Cecil",
+                   LastName = "O'Hegarthy",
+                   DateOfBirth = new DateTime(1990,6,12),
+                   EmailAddress = "CecilOHegarthy@gmail.com",
+                   PPSN = "1234567A",
+                   Salary = 49000,
+                   DeskNumber = 2,
                }
             };
-            staff.ForEach(a => context.Staff.Add(a));
+            staff.ForEach(a => context.Staff.AddOrUpdate(a));
             context.SaveChanges();
+
+            var buildings = new List<Building>
+            {
+                new Building
+                {
+                    SafeOffice = "Gandon PSC Centre",
+                    AddressLine1 = "PSC Gandon",
+                    AddressLine2 = "Public Services Card Centre",
+                    AddressLine3 = "Gandon House",
+                    AddressLine4 = "Amiens Street ",
+                    County = "Dublin",
+                    Phone = "(01) 8172640"
+                }
+            };
+            buildings.ForEach(b => context.Buildings.AddOrUpdate(b));
+            context.SaveChanges();
+
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    PersonId = 3,
+                    FirstName = "Adam",
+                    LastName = "Buckley",
+                    DateOfBirth = new DateTime(1996, 12, 2),
+                    EmailAddress = "Adam@gmail.com",
+                    PPSN = "1234567S",
+                    CivilStatus = "Single"
+                },
+                new Customer
+                {
+                    PersonId = 4,
+                    FirstName = "Amanda",
+                    LastName = "Chapington",
+                    DateOfBirth = new DateTime(1987,5,12),
+                    EmailAddress = "Adam@gmail.com",
+                    PPSN = "1234567A",
+                    CivilStatus = "Married"
+                }
+            };
+            customers.ForEach(c => context.Customers.AddOrUpdate(c));
+            context.SaveChanges();
+
+            var appointments = new List<Appointment>
+            {
+                new Appointment
+                {
+                    BuildingId = 1, // Gandon
+                    StaffId = 1, // Daniel Buckley
+                    CustomerId = 3, // Adam Buckley,
+                    DateOfAppointment = DateTime.Today.Date,
+                    TimeOfAppointment = DateTime.Now.AddHours(2)
+                },
+                new Appointment
+                {
+                    BuildingId = 1, // Gandon
+                    StaffId = 2, // Cecil
+                    CustomerId = 4, // Amanda Chapington,
+                    DateOfAppointment = DateTime.Today.Date,
+                    TimeOfAppointment = DateTime.Now.AddHours(4)
+                }
+            };
         }
     }
 }
