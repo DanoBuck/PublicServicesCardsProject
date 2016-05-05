5c11291af005de096160f9c5ccc686931ea04995
DanoBuck <X00109141@gmail.com>
2016-05-05 23:44:37 +0100

    Added a JS Script to Correctly Populate Drop Down List, Unfortunately it Doesn't Work, Need to Find Solution

---
+++ PublicServicesCardsProject/Scripts/CustomJavaScript.js
@@ -0,0 +1,26 @@
+﻿/*
+* This code is based on that of found at http://www.devcurry.com/2013/01aspnet-mvc-cascading-dropdown-list.html
+* On 5/5/2016                                       
+*/
+$(document).ready(function () {
+    $("#BuildingId").change(function () {
+        $("#StaffId").empty();
+        $.ajax({
+            type: 'POST',
+            url: '@Url.Action("GetAllStaffInBuilding")',
+            dataType: 'json',
+            data: { id: $("#BuildingId").val() },
+            success: function (staffs) {
+                $.each(staffs, function (i, staff) {
+                    $("#StaffId").append('<option value ="'
+                        + staff.StaffId + '">'
+                        + staff.Name + '</option');
+                });
+            },
+            error: function (ex) {
+                alert('Failed to complete!' + ex);
+            }
+        });
+        return false;
+    })
+});
