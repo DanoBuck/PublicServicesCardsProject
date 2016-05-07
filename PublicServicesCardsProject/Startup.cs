1719d4a802083009937f971fb0aca8e9b2508cb7
DanoBuck <X00109141@gmail.com>
2016-05-07 11:57:15 +0100

    Added Roles and Users to Database in Startup.cs

--- PublicServicesCardsProject/Startup.cs
+++ PublicServicesCardsProject/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PublicServicesCardsProject.Models;
 
 [assembly: OwinStartupAttribute(typeof(PublicServicesCardsProject.Startup))]
 namespace PublicServicesCardsProject
@@ -9,6 +12,75 @@ namespace PublicServicesCardsProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        public void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "DaniielBuckleyTy3@gmail.com";
+                user.Email = user.UserName;
+
+                string password = "P@ssWord1'";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Manager");
+                }
+            }
+
+            if (!roleManager.RoleExists("Staff"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Staff";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Staff@gmail.com";
+                user.Email = user.UserName;
+
+                string password = "P@ssWord1'";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Staff");
+                }
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Customer@gmail.com";
+                user.Email = user.UserName;
+
+                string password = "P@ssWord1'";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Customer");
+                }
+            }
         }
     }
 }
