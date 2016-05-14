7d066dd3d205c46bb09be48cddfdce29f7795c58
DanoBuck <X00109141@gmail.com>
2016-05-14 11:34:27 +0100

    Register Sorted, User is now Registered as a Customer and also sees their Correct View

--- PublicServicesCardsProject/Controllers/AccountController.cs
+++ PublicServicesCardsProject/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace PublicServicesCardsProject.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -35,9 +35,9 @@ namespace PublicServicesCardsProject.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -121,7 +121,7 @@ namespace PublicServicesCardsProject.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -152,12 +152,27 @@ namespace PublicServicesCardsProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    Customers = new Customer
+                    {
+                        PPSN = model.Customers.PPSN,
+                        FirstName = model.Customers.FirstName,
+                        LastName = model.Customers.LastName,
+                        EmailAddress = model.Email,
+                        DateOfBirth = model.Customers.DateOfBirth,
+                        CivilStatus = model.Customers.CivilStatus
+                    }
+                };
+                user.CustomerId = user.Customers.CustomerId;
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     UserManager.AddToRole(user.Id, "Customer"); // Now Users Registering are Customers by Default
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
--- PublicServicesCardsProject/Models/AccountViewModels.cs
+++ PublicServicesCardsProject/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace PublicServicesCardsProject.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public virtual Customer Customers { get; set; }
     }
 
     public class ResetPasswordViewModel
