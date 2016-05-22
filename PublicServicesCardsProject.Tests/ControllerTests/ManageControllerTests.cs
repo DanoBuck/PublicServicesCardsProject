01b06d5697681d81f62398694a0e8e553b47fdf6
DanoBuck <X00109141@gmail.com>
2016-05-22 01:06:53 +0100

    Some More Unit Tests

---
+++ PublicServicesCardsProject.Tests/ControllerTests/ManageControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicServicesCardsProject.Controllers;
+using System.Web.Mvc;
+
+namespace PublicServicesCardsProject.Tests.ControllerTests
+{
+    [TestClass]
+    public class ManageControllerTests
+    {
+        private ManageController controller;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            controller = new ManageController();
+        }
+
+        [TestMethod]
+        public void TestPhoneNumberGet()
+        {
+            var result = controller.AddPhoneNumber();
+            result.Should().BeOfType(typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void TestChangePasswordGet()
+        {
+            var result = controller.ChangePassword();
+            result.Should().BeOfType(typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void TestSetPasswordGet()
+        {
+            var result = controller.SetPassword();
+            result.Should().BeOfType(typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void TestLinkLoginPost()
+        {
+            controller.Dispose();
+        }
+    }
+}
---
+++ PublicServicesCardsProject.Tests/MigrationTests/InitialCreateTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicServicesCardsProject.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicServicesCardsProject.Tests.MigrationTests
+{
+    [TestClass]
+    public class InitialCreateTests
+    {
+        private InitialCreate initialCreate;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            initialCreate = new InitialCreate();
+        }
+
+        [TestMethod]
+        public void TestUp()
+        {
+            initialCreate.Up();
+        }
+
+        [TestMethod]
+        public void TestDown()
+        {
+            initialCreate.Down();
+        }
+    }
+}
